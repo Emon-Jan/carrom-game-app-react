c2a5f02915a3521c7687c496439c7bb069e98847
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-01-07 16:22:48 +0600

    animation added

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,37 +1,74 @@
 import React, { Component } from "react";
 import "./Game.css"
 
-
+let ctx;
+let canvasRef;
+let requestId;
+let x = 50;
+let dx = 3;
 class Game extends Component {
 
-    state = {
-        bird: {
-            radius: 20
-        }
+    constructor(props) {
+        super(props);
+        this.canvas = React.createRef();
     }
 
     draw = (ctx, x, y, colour) => {
-        // change this colour to change the colour of your 
-        // "pen" in the canvas 
         ctx.fillStyle = colour;
         ctx.beginPath();
-        ctx.arc(x, y, this.state.bird.radius, 0, 2 * Math.PI);
+        ctx.arc(x, y, this.props.bird.radius, 0, 2 * Math.PI, false);
         ctx.fill();
         ctx.stroke();
     }
 
     componentDidMount() {
-        const ctx = this.refs.canvas.getContext("2d");
-        this.refs.canvas.width = window.innerWidth;
-        this.refs.canvas.height = window.innerHeight;
+        canvasRef = this.canvas.current;
+        ctx = canvasRef.getContext("2d");
+        canvasRef.width = (window.innerWidth * (75 / 100));
+        canvasRef.height = (window.innerHeight * (75 / 100));
+
+        this.draw(ctx, 50, 100, "red");
+        this.draw(ctx, 50, 160, "black");
+    }
+
+    componentDidUpdate() {
+        canvasRef.width = (window.innerWidth * (75 / 100));
+        canvasRef.height = (window.innerHeight * (75 / 100));
+    }
+
+
+    displayWork = () => {
+        ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+        x += dx;
+        if ((x + this.props.bird.radius) > canvasRef.width || x < this.props.bird.radius) {
+            dx = -dx;
+        }
+        this.draw(ctx, x, 100, "red");
+        this.draw(ctx, x, 160, "black");
+        requestId = requestAnimationFrame(this.displayWork);
+    }
+
+    cancel = () => {
+        cancelAnimationFrame(requestId);
+        ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+        x = 50;
+        dx = 3;
         this.draw(ctx, 50, 100, "red");
         this.draw(ctx, 50, 160, "black");
     }
 
     render() {
         return (
-            <div>
-                <canvas ref="canvas" />
+            <div className="game-top">
+                <div>
+                    <button onClick={this.displayWork}>Animate</button>
+                </div>
+                <div>
+                    <button onClick={this.cancel}>Cancel</button>
+                </div>
+                <div>
+                    <canvas ref={this.canvas} />
+                </div>
             </div>
         );
     }
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,10 +3,18 @@ import Game from '../components/Game'
 import './App.css';
 
 class App extends Component {
+  state = {
+    bird: {
+      radius: 10
+    }
+  }
+
   render() {
     return (
       <div className="App" >
-        <Game />
+        <div>
+          <Game bird={this.state.bird} />
+        </div>
       </div>
     );
   }
