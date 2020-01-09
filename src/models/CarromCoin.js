b7c2f4767d20765dfcc6f9d9f2aa7bacbd162b2f
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-01-09 17:09:51 +0600

    models created

---
+++ src/models/CarromCoin.js
@@ -0,0 +1,26 @@
+import GameObject from "./GameObject";
+
+class CarromCoin extends GameObject {
+    constructor(context, x, y, radius, color, vx, vy) {
+        super(context, x, y, radius, color, vx, vy);
+
+        this.color = color;
+        this.radius = radius;
+    }
+
+    draw = () => {
+        this.context.fillStyle = this.color;
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        this.context.fill();
+        this.context.stroke();
+    }
+
+    update = (secondsPassed) => {
+        //Move with set velocity
+        this.x += this.vx * secondsPassed;
+        this.y += this.vy * secondsPassed;
+    }
+}
+
+export default CarromCoin;
