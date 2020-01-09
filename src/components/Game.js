f2f65d527d1aee64039ba723b159453d9d8a45ee
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-01-09 08:54:49 +0600

    board update

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,8 +4,8 @@ import "./Game.css"
 let ctx;
 let canvasRef;
 let requestId;
-let x = 50;
-let dx = 3;
+let requestIdForMouse;
+let x = 0;
 class Game extends Component {
 
     constructor(props) {
@@ -18,70 +18,74 @@ class Game extends Component {
 
     }
 
-    draw = (ctx, x, y, colour) => {
-        ctx.fillStyle = colour;
-        ctx.beginPath();
-        ctx.arc(x, y, this.props.bird.radius, 0, 2 * Math.PI, false);
-        ctx.fill();
-        ctx.stroke();
+    draw = (x, y, radius, color, items) => {
+        for (let i = 0; i < items; i++) {
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.stroke();
+            x += radius * 2;
+            y += radius * 2;
+        }
     }
 
     componentDidMount() {
         canvasRef = this.canvas.current;
         ctx = canvasRef.getContext("2d");
-        canvasRef.width = (window.innerWidth * (75 / 100));
-        canvasRef.height = (window.innerHeight * (75 / 100));
+        canvasRef.width = 600;
+        canvasRef.height = 600;
 
-        this.draw(ctx, 50, 100, "red");
-        this.draw(ctx, 50, 160, "black");
+        // this.draw(ctx, 50, 100, this.props.bird.radius, "red");
+        // this.draw(ctx, 50, 160, this.props.bird.radius, "black");
 
         document.addEventListener("mousemove", (event) => {
-            this.mouse.x = event.clientX;
-            this.mouse.y = event.clientY;
+            var rect = canvasRef.getBoundingClientRect();
+            this.mouse.x = event.x - rect.left;
+            this.mouse.y = event.y - rect.top;
         });
-    }
 
-    componentDidUpdate() {
-        canvasRef.width = (window.innerWidth * (75 / 100));
-        canvasRef.height = (window.innerHeight * (75 / 100));
+        this.displayWork();
+        this.mouseEvent();
     }
 
 
     displayWork = () => {
         ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
-        x += dx;
-        if ((x + this.props.bird.radius) > canvasRef.width || x < this.props.bird.radius) {
-            dx = -dx;
-        }
-        this.draw(ctx, x, 100, "red");
-        this.draw(ctx, x, 160, "black");
+        // x += dx;
+        // if ((x + this.props.bird.radius) > canvasRef.width || x < this.props.bird.radius) {
+        //     dx = -dx;
+        // }
+        this.draw(x, 100, this.props.bird.radius, "MEDIUMSLATEBLUE", 9);
+        this.draw(x, 160, this.props.bird.radius, "DARKSLATEGRAY", 9);
+        this.draw(x, 200, this.props.bird.radius, "RED", 1);
         requestId = requestAnimationFrame(this.displayWork);
     }
 
-    cancel = () => {
-        cancelAnimationFrame(requestId);
-        ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
-        x = 50;
-        dx = 3;
-        this.draw(ctx, 50, 100, "red");
-        this.draw(ctx, 50, 160, "black");
+    mouseEvent = () => {
+        this.draw(this.mouse.x, this.mouse.y, 10, "green", 1);
+        requestIdForMouse = requestAnimationFrame(this.mouseEvent);
     }
+
+    // cancel = () => {
+    //     cancelAnimationFrame(requestId);
+    //     cancelAnimationFrame(requestIdForMouse);
+    //     ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+    //     x = 50;
+    //     // dx = 3;
+    //     this.draw(ctx, 50, 100, "red");
+    //     this.draw(ctx, 50, 160, "black");
+    //     this.draw(ctx, 50, 220, "green");
+    // }
 
     render() {
         return (
             <div className="game-top">
-                <div>
-                    <button onClick={this.displayWork}>Animate</button>
-                </div>
-                <div>
-                    <button onClick={this.cancel}>Cancel</button>
-                </div>
-                <div>
-                    <canvas ref={this.canvas} />
-                </div>
+                <canvas ref={this.canvas} />
             </div>
         );
     }
 }
+// onClick={this.displayWork}
 
 export default Game;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   state = {
     bird: {
-      radius: 10
+      radius: 20
     }
   }
 
